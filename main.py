113b1f83509f34dc3ef5032d3b558c80366dd259
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-28 23:00:23 +0100

    Add corr

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def mul(a):
         mult *= i
     return mult
 def div(a,b):
+    if b == 0:
+        return("Nie można dzielic przez 0 :)")
     logging.debug(f"Dzięlę {a} przez {b}")
     return a / b
 def exp(a,b):
@@ -46,9 +48,9 @@ if __name__ == "__main__":
                 except ValueError:
                     logging.warning(f"Spróbuj podać poprawne liczby\n")
             if action == 1:
-                logging.info(f"Wynik to {add(liczby_int)}")
+                logging.info(f"Wynik to: {add(liczby_int)}")
             elif action == 3:
-                logging.info(f"Wynik to {mul(liczby_int)}")
+                logging.info(f"Wynik to: {mul(liczby_int)}")
         elif action == 2 or action == 4 or action == 5:
             while True:
                 try:
@@ -63,9 +65,9 @@ if __name__ == "__main__":
                 except ValueError:
                     logging.warning(f"To nie jest liczba. Spróbuj ponownie\n")
             if action == 2:
-                logging.info(f"Wynik to {sub(liczba1,liczba2)}")
+                logging.info(f"Wynik to: {sub(liczba1,liczba2)}")
             elif action == 4:
-                logging.info(f"Wynik to {div(liczba1,liczba2)}")
+                logging.info(f"Wynik to: {div(liczba1,liczba2)}")
             elif action == 5:
-                logging.info(f"Wynik to {exp(liczba1,liczba2)}")
+                logging.info(f"Wynik to: {exp(liczba1,liczba2)}")
         con = input("\nCzy chcesz kontunuować? ([T] - tak, [N] - nie): ")
