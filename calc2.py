48aad1a257069020bbc9012ae62d9efc6ce389ee
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-11-29 10:07:41 +0100

    Add corr

--- calc2.py
+++ calc2.py
@@ -28,34 +28,31 @@ def calc(a,b,c):
         logging.debug(f"Dzięlę {b} prze {c}")
         return b/c
 
-print(calc(3,2,[3,4]))
-
-"""
-print("***Kalkulator***")
-con = "t"
-while con == "t" or con == "T":
-    while True:
-        try:
-            action = int(input("Wybierz działanie:\n [1] - dodawanie\n [2] - odejmowanie\n [3] - mnożenie\n [4] - dzielenie\nTwój wybór: "))
-            break
-        except ValueError:
-            logging.warning("Podaj wlaściwą liczbę")
-    if action > 4:
-        logging.warning("Niewłaściwy wybór")
-        con = input("Chcesz spróbowac jeszcze raz? [T] - tak, [N] - nie: ")
-        continue
-    while True:
-        try:
-            liczba1 = float(input("Podaj pierwszą liczbę: "))
-            break
-        except ValueError:
-            logging.warning("Niewłaściwa liczna. Spróbuj jeszcze raz")
-    while True:
-        try:
-            liczba2 = float(input("Podaj drugą liczbę: "))
-            break
-        except ValueError:
-            logging.warning("Niewłaściwa liczba. Spróbuj jeszcze raz")
-    logging.info(f"Wynik: {calc(action,liczba1,liczba2)}")
-    con = input("Kontynuować? [T] - tak, [N] - nie: ")
-"""
+if __name__ == "__main__":
+    print("***Kalkulator***")
+    con = "t"
+    while con == "t" or con == "T":
+        while True:
+            try:
+                action = int(input("Wybierz działanie:\n [1] - dodawanie\n [2] - odejmowanie\n [3] - mnożenie\n [4] - dzielenie\nTwój wybór: "))
+                break
+            except ValueError:
+                logging.warning("Podaj wlaściwą liczbę")
+        if action > 4:
+            logging.warning("Niewłaściwy wybór")
+            con = input("Chcesz spróbowac jeszcze raz? [T] - tak, [N] - nie: ")
+            continue
+        while True:
+            try:
+                liczba1 = float(input("Podaj pierwszą liczbę: "))
+                break
+            except ValueError:
+                logging.warning("Niewłaściwa liczna. Spróbuj jeszcze raz")
+        while True:
+            try:
+                liczba2 = float(input("Podaj drugą liczbę: "))
+                break
+            except ValueError:
+                logging.warning("Niewłaściwa liczba. Spróbuj jeszcze raz")
+        logging.info(f"Wynik: {calc(action,liczba1,liczba2)}")
+        con = input("Kontynuować? [T] - tak, [N] - nie: ")
